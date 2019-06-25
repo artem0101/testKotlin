37f9f3dc840097e0e0f65c5b5f06cc54b913c96d
artem <koryakin1993@inbox.ru>
2019-06-25 17:49:10 +0300

    override methods

--- src/third/Main.kt
+++ src/third/Main.kt
@@ -6,4 +6,33 @@ val map = hashMapOf(1 to "one", 7 to "seven", 53 to "fifty-three")
 
 fun main() {
     println(set.max())
+
+    println(list)
+
+    println(joinToString(list, "; ", "(", ")"))
+
+    println(joinToString(list, separator = " ", prefix = " ", postfix = "."))
+
+    println(joinToString(list, postfix = "&", prefix = "  "))
+
+    println("Kotlin".lastChar())
 }
+
+fun <T> joinToString(
+    collection: Collection<T>,
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+
+    result.append(postfix)
+
+    return result.toString()
+}
+
+fun String.lastChar(): Char = get(length - 1)
